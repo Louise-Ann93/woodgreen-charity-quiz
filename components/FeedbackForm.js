302982c18bbc03fbf3133f82f1688400ed26fe6e
Louise Willoughby <l.ann.willo@googlemail.com>
2022-07-13 21:22:44 +0100

    input boxes

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -10,21 +10,201 @@ export default function FeedbackForm() {
         method="POST"
         action="/success"
       >
-        <input type="hidden" name="form-name" value="feedback" />
+        <input type="hidden" name="form-name" value="pet-quiz" />
         <p className={styles.hidden}>
             <label>
             Don’t fill this out if you’re human: <input name="bot-field" />
             </label>
         </p>
-  
-        <label htmlFor="name">Name</label>
+
+        <p className={styles.p}>Name the Owner 👀 (extra points if you can name them)</p> 
+        <div className={styles.container}>
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
         <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
 
-        <label htmlFor="email">Email</label>
-        <input id="email" className={styles['form-field']} type="email" name="email" required />
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
 
-        <label htmlFor="feedback">What is your feedback?</label>
-        <textarea id="feedback" className={styles['form-field']} wrap="soft" name="feedback" required></textarea>
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        <div>
+        <div>Image Here</div>
+        <label htmlFor="name"></label>
+        <input id="name" className={styles['form-field']} type="text" name="name" />
+        </div>
+
+        </div>
+
+        <p className={styles.p}>Question Round!</p> 
+        <div className={styles.container}>
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        <div>
+        <div>Question</div>
+        <label htmlFor="email"></label>
+        <input id="email" className={styles['form-field']} type="email" name="email"/>
+        </div>
+
+        </div>
+
         <button className={styles.button} type="submit">Submit</button>
       </form>
   )
